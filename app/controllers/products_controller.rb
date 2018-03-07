50c694095afcaf45b9bce48c7d5384db890b21dd
Peter Jang <peter.x.jang@gmail.com>
2018-03-06 20:22:35 -0600

    Add routes for individual product urls

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,19 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products.as_json
   end
+
+  def first_product_method
+    product = Product.first
+    render json: product.as_json
+  end
+
+  def second_product_method
+    product = Product.second
+    render json: product.as_json
+  end
+
+  def third_product_method
+    product = Product.third
+    render json: product.as_json
+  end
 end
