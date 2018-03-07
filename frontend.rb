adf1ef2d8a61cb024a7ed3b035fa63db6e6d023c
Peter Jang <peter.x.jang@gmail.com>
2018-03-06 22:33:02 -0600

    Add table output using tty-table gem

--- frontend.rb
+++ frontend.rb
@@ -1,5 +1,11 @@
 require "unirest"
+require "tty-table"
 
 response = Unirest.get("http://localhost:3000/v1/all_products_url")
 products = response.body
 puts JSON.pretty_generate(products)
+
+table_header = ["Id", "Name", "Price"]
+table_body = products.map { |product| [product["id"], product["name"], product["price"]]}
+table = TTY::Table.new table_header, table_body
+puts table.render(:ascii)
