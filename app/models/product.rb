86e7e7737e60ebfd712a0403a53894b1d7e3b809
Peter Jang <peter.x.jang@gmail.com>
2018-03-12 20:26:12 -0500

    Add custom json response using model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,26 @@
 class Product < ApplicationRecord
+  def is_discounted
+    price <= 2
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
+
+  def as_json
+    {
+      id: id,
+      name: name,
+      description: description,
+      image_url: image_url,
+      price: price,
+      tax: tax,
+      total: total,
+      is_discounted: is_discounted
+    }
+  end
 end
